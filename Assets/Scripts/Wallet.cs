856682937c101b4e268c83ca284d5a8ba475358d
Jonny <53932961+Project-Jonny@users.noreply.github.com>
2024-05-03 22:25:41 +0900

    #10 財布の作成

---
+++ Assets/Scripts/Wallet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wallet : MonoBehaviour
+{
+    [SerializeField] Text coinText;
+
+    int maxCoin = 500;
+    float nowCoin = 0;
+
+    int coinSpeed = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinText.text = nowCoin.ToString() + "/" + maxCoin.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.instance.isGame && nowCoin <= maxCoin)
+        {
+            nowCoin += Time.deltaTime * coinSpeed;
+            coinText.text = nowCoin.ToString("F0") + "/" + maxCoin.ToString();
+        }
+    }
+}
